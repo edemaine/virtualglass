e7738f6e78a224edaca80db493a31dddb1a814f8
Andrew Winslow <andrewwinslow@gmail.com>
2011-06-06 15:53:38 +0000

    added documentation for OpenGLWidget

--- openglwidget.cpp
+++ openglwidget.cpp
@@ -1,3 +1,41 @@
+/*
+This class is the QT GUI object that does 3D rendering.
+It also responds to mouse clicks and mouse movement within
+its extent.
+
+The actual computation of the cane mesh is handled by the 
+Mesh object defined in mesh.h/cpp. This class only requests
+a pointer to the mesh and draws it using normal OpenGL calls.
+
+As this object handles mouse clicks and movement, it is 
+involved in modifying the cane. For example, if the user
+is in twist mode and moves the mouse horizontally, this
+object tells the mesh object ``Hey, the cane has been twisted
+by amount X''. 
+
+Features:
+
+DUAL RESOLUTION:
+Two mesh resolutions are used to give an improved interaction
+experience. A LOW_RESOLUTION mode is used when the cane is undergoing
+motion (as determined by whether the user currently is holding 
+the mouse down), while a HIGH_RESOLUTION mode is used otherwise.
+The idea is to give good framerate during movement in trade for 
+a higher resolution (as a lower resolution is not as noticeable for
+moving objects).
+
+MODES:
+The OpenGLWidget object has a mode for each type of modification 
+the user can make to the cane (in addition to a LOOK_MODE, which
+changes the orientation of the cane only). Changing modes is handled by
+other parts of the GUI, and a changed mode is indicated to the 
+OpenGLWidget object by calling setMode(). Depending upon the mode,
+mouse movement/clicks cause different behavior. For example, 
+dragging the mouse horizontally while in TWIST_MODE causes the cane
+to become twisted (left for CW, right for CCW), while moving the mouse
+vertically in stretch mode causes the cane to stretch (up for increased
+stretch, down for decreased).
+*/
 
 #include "openglwidget.h" 
  
@@ -11,27 +49,35 @@ OpenGLWidget :: OpenGLWidget(QWidget *parent=0) : QGLWidget(parent)
 
 void OpenGLWidget :: initializeGL()
 {
+        // Camera location
         theta = -PI/2.0;
         fee = PI/4.0;
         rho = 3.0;
 
+        // Camera look-at location
+        lookAtLoc[0] = lookAtLoc[1] = 0;
+        lookAtLoc[2] = 0.5;
+
+        // For shadow/lighting
         lightPosition[0] = 0.0;
         lightPosition[1] = 0.0;
         lightPosition[2] = 1000.0;
         lightPosition[3] = 0.0;
-
-        lookAtLoc[0] = lookAtLoc[1] = 0;
-        lookAtLoc[2] = 0.5;
-
         glEnable(GL_LIGHTING);
         glEnable(GL_LIGHT0);
         glEnable(GL_COLOR_MATERIAL);
         glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 
+        // For transparency
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_DST_ALPHA);
 }
- 
+
+/*
+Resets the cane currently being operated on to ``nothing''.
+This immediately causes the rendering to be updated to a blank
+region (no triangles exist to be drawn).
+*/ 
 void OpenGLWidget :: zeroCanes()
 {
         mesh->setCane(NULL);
@@ -39,6 +85,12 @@ void OpenGLWidget :: zeroCanes()
         paintGL();
 } 
 
+/*
+Handles the drawing of a triangle mesh.
+The triangles array is created and lives in the
+Mesh object, and the OpenGLWidget object simply
+receives a pointer to this array. 
+*/
 void OpenGLWidget :: paintGL() 
 {
         int i;
@@ -54,7 +106,10 @@ void OpenGLWidget :: paintGL()
 
         swapBuffers();  
 }  
- 
+
+/*
+Calls if the OpenGLWidget object is resized (in the GUI sense).
+*/ 
 void OpenGLWidget :: resizeGL(int width, int height)
 {
         glViewport(0, 0, width, height);
@@ -79,6 +134,12 @@ void OpenGLWidget :: zoomOut()
         paintGL();
 }
 
+/*
+Sets the cane currently being interacted with to
+a new cane. This is achieved by passing the cane off
+to the Mesh object, getting a pointer back to a Triangle
+array containing the mesh, and drawing it.
+*/
 void OpenGLWidget :: setFocusCane(Cane* c)
 {
         mesh->setCane(c);
@@ -86,38 +147,62 @@ void OpenGLWidget :: setFocusCane(Cane* c)
         paintGL();
 } 
 
-
+/*
+Is called when the resolution of the rendered cane is changed.
+Two resolution modes are available: LOW_RESOLUTION and HIGH_RESOLUTION.
+The method simply updates the `resolution' instance variable and
+requests the (possibly new) resolution of the cane from the Mesh 
+object, updating its Triangle array pointer.
+*/
 void OpenGLWidget :: updateResolution(int new_resolution)
 {
         resolution = new_resolution;
-        triangles = mesh->getMesh(new_resolution);
-        num_triangles = mesh->getNumMeshTriangles(new_resolution);
+        updateTriangles();
 }
 
+/*
+Should be called any time the Mesh object is changed by
+modifying the cane it contains. 
+*/
 void OpenGLWidget :: updateTriangles()
 {
         triangles = mesh->getMesh(resolution);
         num_triangles = mesh->getNumMeshTriangles(resolution);
 } 
 
+/*
+Updates the mode. See constants.h for the various
+modes available (named *_MODE).
+*/
 void OpenGLWidget :: setMode(int m)
 {
         this->mode = m;
-        if (mode == 4)
+
+        // Special call when entering BUNDLE_MODE
+        // because the mode causes the cane to
+        // change appearance (subcane illumination, etc.).
+        if (mode == BUNDLE_MODE)
         {
-                mesh->startMoveMode();
+                mesh->startMoveMode(); 
         }
 }
 
+/*
+Changes the current subcane undergoing change to
+the next subcane in the cane.
+*/
 void OpenGLWidget :: advanceActiveSubcane()
 {
-        if (mode == 4)
+        if (mode == BUNDLE_MODE)
         {
                 mesh->advanceActiveSubcane();
                 paintGL();
         }
 }
 
+/*
+Called after the camera changes location.
+*/
 void OpenGLWidget :: updateCamera()
 {
         eyeLoc[0] = lookAtLoc[0] + rho*sin(fee)*cos(theta);
@@ -129,22 +214,40 @@ void OpenGLWidget :: updateCamera()
                 0.0, 0.0, 1.0);   
 }
 
+/*
+Currently catches all mouse press events 
+(left and right buttons, etc.).
+*/
 void OpenGLWidget :: mousePressEvent (QMouseEvent* e)
 {
-        //----Init new location
+        // Update instance variables for mouse location
         mouseLocX = e->x();
         mouseLocY = e->y();
 
-        updateResolution(LOW_RESOLUTION);
+        // Change as part of dual mode feature
+        updateResolution(LOW_RESOLUTION); 
+
         paintGL();
 }
 
+/*
+Currently catches all mouse release events 
+(left and right buttons, etc.).
+*/
 void OpenGLWidget :: mouseReleaseEvent (QMouseEvent* e)
 {
+        // Change as part of dual mode feature
         updateResolution(HIGH_RESOLUTION);
+
         paintGL();
 }
 
+/*
+This method is an event handler called when the mouse
+is moved *and a button is down*. Depending on the mode,
+the cane (and view of the cane) changes. This is
+part of the mode feature.
+*/
 void OpenGLWidget :: mouseMoveEvent (QMouseEvent* e)
 {
         float relX, relY;
@@ -155,22 +258,27 @@ void OpenGLWidget :: mouseMoveEvent (QMouseEvent* e)
         windowWidth = this->width();
         windowHeight = this->height();
 
-        //----calculate change in mouse x
+        // Calculate how much mouse moved
         oldMouseLocX = mouseLocX;
         mouseLocX = e->x();
         relX = (mouseLocX - oldMouseLocX) / windowWidth;
-
-        //----calculate change in mouse y
         oldMouseLocY = mouseLocY;
         mouseLocY = e->y();
         relY = (mouseLocY - oldMouseLocY) / windowHeight;
 
-        //----calculate change in mouse r and theta
+        /* 
+        Do something depending on mode.
+        All modes except LOOK_MODE involve modifying the cane
+        itself, while LOOK_MODE moves the camera.
 
+        All of the calls to mesh->*Cane() are functions of relX/relY,
+        but the constants involved are determined by experiment, 
+        i.e. how much twist `feels' reasonable for moving the mouse 
+        an inch.
+        */
         if (mode == LOOK_MODE)
         {
-                //rotate eye position around lookat point
-                //only rotate in one direction at a time
+                // Rotate camera position around look-at location.
                 theta -= (relX * 500.0 * PI / 180.0);
                 newFee = fee - (relY * 500.0 * PI / 180.0);
                 if (newFee > 0.0f && newFee < PI)
@@ -189,6 +297,17 @@ void OpenGLWidget :: mouseMoveEvent (QMouseEvent* e)
         }
         else if (mode == BUNDLE_MODE)
         {
+                /*
+                How the parameters for moveCane() are calculated is not obvious.
+                The idea is to make mouse X/Y correspond to the cane moving 
+                left-right/up-down *regardless* of where the camera is. This
+                is why theta (the camera angle relative to the look-at point) is
+                also involved. 
+      
+                Essentially, the parameters convert the amount moved in X and Y
+                (variables `relX' and `relY') to the amount moved in X and Y
+                according to axes on which the cane lives.
+                */
                 mesh->moveCane(relX * cos(theta + PI / 2.0) + relY * cos(theta), 
                         relX * sin(theta + PI / 2.0) + relY * sin(theta));
                 updateTriangles();
@@ -202,7 +321,11 @@ void OpenGLWidget :: mouseMoveEvent (QMouseEvent* e)
         paintGL();
 }
 
-
+/*
+Convert a triangle in 3D to stuff OpenGL needs to draw it,
+namely its normal vector. Assumes the triangle is specified 
+in CCW order relative to its exterior.
+*/
 void OpenGLWidget :: drawTriangle(Triangle* t)
 {
         Point vec1, vec2, norm;
@@ -232,8 +355,17 @@ void OpenGLWidget :: drawTriangle(Triangle* t)
         glEnd();
 }
 
+/*
+Places an additional cane (given as an argument)
+into the existing cane being operated on.
+*/
 void OpenGLWidget :: addCane(Cane* c)
 {
+        /*
+        If there is no cane currently being operated on,
+        start off by looking at it, otherwise start off by
+        adjusting its location.
+        */
         if (mesh->getCane() == NULL)
                 setMode(LOOK_MODE); 
         else
@@ -243,6 +375,10 @@ void OpenGLWidget :: addCane(Cane* c)
         paintGL();
 }
 
+/*
+Return a pointer to the cane curently being operated on.
+DO NOT CHANGE THIS CANE, you will make the Mesh object cry.
+*/
 Cane* OpenGLWidget :: getCane()
 {
         return mesh->getCane();
