54f49dcb9d33c79086c4eea5b1f1960453fe77da
Erik Demaine <edemaine@mit.edu>
2011-08-25 14:35:53 +0000

    note about not working

--- windows-make.bat
+++ windows-make.bat
@@ -1,3 +1,4 @@
+echo WARNING: This script doesn't seem to work yet.  Try QtBuilder instead.
 "C:\Program Files\QtSDK\Simulator\Qt\mingw\bin\qmake.exe"
 set PATH=C:\Program Files\QtSDK\mingw\bin
 mingw32-make
