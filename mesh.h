6bdd01f0ec73348e91467563e74cf6f57accfd25
Andrew Winslow <andrewwinslow@gmail.com>
2011-10-16 03:10:39 +0000

    3D view now work to great victory

---
+++ mesh.h
@@ -0,0 +1,20 @@
+
+
+#ifndef MESH_H
+#define MESH_H
+
+#include <vector.h>
+#include "pullplan.h"
+#include "geometry.h"
+
+void generateMesh(PullPlan* plan, Geometry *geometry, vector<PullPlan*> ancestors, vector<int> ancestorIndices, int groupIndex = -1);
+void meshPolygonalBaseCane(Geometry* geometry, vector<PullPlan*> ancestors, vector<int> ancestorIndices, PullPlan* plan, uint32_t group_tag);
+void applyMoveAndResizeTransform(Vertex* v, PullPlan* parentPlan, int subplan);
+void applyMoveAndResizeTransform(Geometry* geometry, PullPlan* parentPlan, int subplan);
+void applyTwistTransform(Vertex* v, PullPlan* p);
+void applyTwistTransform(Geometry* geometry, PullPlan* p);
+Vertex applyTransforms(Vertex p, PullPlan** ancestors, int ancestorCount, bool full);
+
+
+#endif
+
