b61f74df228c6aa2629d75e14b1c000608fa47c9
Erik Demaine <edemaine@mit.edu>
2012-06-14 16:41:20 +0000

    abort on failure

--- make-version.sh
+++ make-version.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-echo Subversion: `svnversion`
-echo Date: `date +%F`
+if ! svnversion
+then
+  ## Abort if svnversion fails (e.g. wrong version of Subversion)
+  exit 1
+fi
+if ! date +%F
+then
+  ## Abort if date fails (e.g. no date program)
+  exit 1
+fi
 (
   svnversion
   date +%F
