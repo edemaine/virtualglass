9f48de7786c2d595c9f36f212398b08a6537f894
Andrew Winslow <andrewwinslow@gmail.com>
2012-06-22 22:01:16 +0000

    downsized casing and underlay color targets slightly

--- overlaycolorwidget.cpp
+++ overlaycolorwidget.cpp
@@ -4,7 +4,7 @@
 OverlayColorWidget :: OverlayColorWidget(Color** color, QWidget* parent): QWidget(parent)
 {
         setAcceptDrops(true);
-	setFixedSize(30, 30);
+	setFixedSize(20, 20);
 
 	this->color = color;
 }
@@ -13,10 +13,10 @@ void OverlayColorWidget :: paintEvent(QPaintEvent*)
 {
 	QPainter painter;
 	painter.begin(this);
-        painter.fillRect(QRect(0, 0, 30, 30), QColor(0, 0, 0, 255));
-        painter.fillRect(QRect(1, 1, 28, 28), QColor(255, 255, 255, 255));
+        painter.fillRect(QRect(0, 0, 20, 20), QColor(0, 0, 0, 255));
+        painter.fillRect(QRect(1, 1, 18, 18), QColor(255, 255, 255, 255));
 	Color* c = (*(this->color));
-        painter.fillRect(QRect(1, 1, 28, 28), QColor(255*c->r, 255*c->g, 255*c->b, 255*c->a));
+        painter.fillRect(QRect(1, 1, 18, 18), QColor(255*c->r, 255*c->g, 255*c->b, 255*c->a));
         painter.end();
 }
 
