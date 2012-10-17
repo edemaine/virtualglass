5889f8bf1e4dcce6ed2ec2843e66d6333a884043
Andrew Winslow <andrewwinslow@gmail.com>
2012-10-17 15:39:22 +0000

    OOPS forgot to add GlobalGlass files :x

---
+++ globalglass.cpp
@@ -0,0 +1,35 @@
+
+#include "globalglass.h"
+#include "pullplan.h"
+#include "glasscolor.h"
+#include "pulltemplate.h"
+#include "shape.h"
+
+namespace GlobalGlass
+{
+        GlassColor* _color = 0;
+        PullPlan* _circlePlan = 0;
+        PullPlan* _squarePlan = 0;
+
+        GlassColor* color()
+	{
+		if (_color == 0)
+			_color = new GlassColor();
+		return _color;
+	}
+	
+        PullPlan* circlePlan()
+	{
+		if (_circlePlan == 0)
+			_circlePlan = new PullPlan(PullTemplate::BASE_CIRCLE);
+		return _circlePlan;
+	}
+
+        PullPlan* squarePlan()
+	{
+		if (_squarePlan == 0)
+			_squarePlan = new PullPlan(PullTemplate::BASE_CIRCLE);
+		return _squarePlan;
+	}
+}
+
---
+++ globalglass.h
@@ -0,0 +1,24 @@
+
+#ifndef GLOBALGLASS_H
+#define GLOBALGLASS_H 
+
+class GlassColor;
+class PullPlan;
+
+namespace GlobalGlass
+{
+	// call these, as they're guaranteed to get you something valid.
+	// there is risk for infinite recursion, as calling circlePlan()
+	// or squarePlan can trigger the creation of a new pull plan, 
+	// which might call these, etc. This is avoided because the pull plans
+	// created by circlePlan() and squarePlan() calls have no subplans, 
+	// so *they do not call circlePlan() and squarePlan()*.
+	// Hopefully this doesn't change o_o
+	GlassColor* color();
+	PullPlan* circlePlan();
+	PullPlan* squarePlan();
+
+}
+
+#endif
+
