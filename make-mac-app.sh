9185718a20c1b33ae7f1694a128504e4e64943e7
Andrew Winslow <andrewwinslow@gmail.com>
2013-04-19 01:50:44 +0000

    a couple of script changes based on packaging stuff up

--- make-mac-app.sh
+++ make-mac-app.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
-bash make-version.sh
+
+# This script assumes you compiled Qt statically.
+#
+# Run otool -L ./virtualglass.app/Contents/MacOS/virtualglass
+# to see if you're using @executable_path (static) Qt libraries, 
+# or /System/Library (shared) Qt libraries. Using static ones requires
+# a static installation of Qt (see many online tutorials for how).
+
+# update version file
+#bash make-version.sh
+# recompile for release
+rm -rf ./virtualglass.app/
+rm -rf ./Virtual\ Glass.app/
 qmake -spec macx-g++ -config release virtualglass.pro
-rm -r ./virtualglass.app
+make clean
 make
-/usr/bin/macdeployqt-4.8 ./virtualglass.app/
+# copy our custom OS config file for file type associations, etc.
 cp ./virtualglass.Info.plist ./virtualglass.app/Contents/Info.plist
+# rename the app
 mv ./virtualglass.app/ ./Virtual\ Glass.app/ 
+
+# reset compilation setup to development settings
 qmake -spec macx-g++ -config debug virtualglass.pro
 
-# User-readable instructions 
-# How to create a standalone executable on Mac OS X:
-# 1. Compile in the normal way:
-# 1a. `qmake -spec macx-g++ -config release virtualglass.pro'
-# 1b. rm -r ./virtualglass.app
-# 1c. `make'
-# 2. Modify the library references to be internal:
-# `/usr/bin/macdeployqt-4.8 ./virtualglass.app/'
-# 3. Add the fancy Info.plist file for .glass and .vgc file
-# associations (among other things).
-
-# Links related to old bug in macdeployqt-4.7 (no longer used):
-# http://wiki.oz9aec.net/index.php/Packaging_Qt_apps_for_Mac_OS_X
-# http://www.pyinstaller.org/ticket/157
 
 
 
