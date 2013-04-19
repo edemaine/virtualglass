2c9b83d71b8a7a808c28f484691eb13f0f2a60e6
Andrew Winslow <andrewwinslow@gmail.com>
2013-04-19 02:42:04 +0000

    updated release text info for the website

--- make-mac-app.sh
+++ make-mac-app.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-# This script assumes you compiled Qt statically.
-#
-# Run otool -L ./virtualglass.app/Contents/MacOS/virtualglass
-# to see if you're using @executable_path (static) Qt libraries, 
-# or /System/Library (shared) Qt libraries. Using static ones requires
-# a static installation of Qt (see many online tutorials for how).
-
 # update version file
-#bash make-version.sh
-# recompile for release
+bash make-version.sh
+# recreate makefiles for release
+qmake -spec macx-g++ -config release virtualglass.pro
+# clean everything and recompile
 rm -rf ./virtualglass.app/
 rm -rf ./Virtual\ Glass.app/
-qmake -spec macx-g++ -config release virtualglass.pro
 make clean
 make
 # copy our custom OS config file for file type associations, etc.
 cp ./virtualglass.Info.plist ./virtualglass.app/Contents/Info.plist
 # rename the app
-mv ./virtualglass.app/ ./Virtual\ Glass.app/ 
+mv ./virtualglass.app/ ./VirtualGlass.app/ 
+# fix libraries
+# macdeployqt is kind of buggy and sort of magic, so this can fail.
+# we use -no-strip because of errors that occur when trying to strip out unused libraries.
+/usr/bin/macdeployqt ./VirtualGlass.app/ -no-strip
 
 # reset compilation setup to development settings
 qmake -spec macx-g++ -config debug virtualglass.pro
