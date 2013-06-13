dd23f43d0df6e5d7520ad3be9681073149eb9ef4
Andrew Winslow <andrewwinslow@gmail.com>
2013-06-13 02:54:10 +0000

    updated release text

--- make-mac-app.sh
+++ make-mac-app.sh
@@ -16,7 +16,8 @@ mv ./virtualglass.app/ ./VirtualGlass.app/
 # fix libraries
 # macdeployqt is kind of buggy and sort of magic, so this can fail.
 # we use -no-strip because of errors that occur when trying to strip out unused libraries.
-/usr/bin/macdeployqt ./VirtualGlass.app/ -no-strip
+# also, you might have to run this as superuser, depending on the libraries and permissions on them
+/opt/local/bin/macdeployqt ./VirtualGlass.app/ -no-strip
 
 # reset compilation setup to development settings
 qmake -spec macx-g++ -config debug virtualglass.pro
