b045a3ce603464d7b98ff315b2c74c09304e0be4
Andrew Winslow <andrewwinslow@gmail.com>
2013-08-15 18:28:26 +0000

    removed intermediate mesh resolution rendering

--- piecegeometrythread.cpp
+++ piecegeometrythread.cpp
@@ -19,8 +19,6 @@ void PieceGeometryThread::run()
 
 	while (1)
 	{
-		sleep:
-
 		pew->wakeWait.wait(&(pew->wakeMutex));
 		
 		compute:
@@ -31,55 +29,6 @@ void PieceGeometryThread::run()
 		pew->tempPieceDirty = false;
 		pew->tempPieceMutex.unlock();	
 		
-		// compute the low-res geometry
-		pew->tempGeometryMutex.lock();
-		completed = generateMesh(myTempPiece, &(pew->tempPieceGeometry), &(pew->tempPickupGeometry), 
-			GlobalGraphicsSetting::VERY_LOW);
-		pew->tempGeometryMutex.unlock();	
-		pew->geometryDirtyMutex.lock();
-		pew->geometryDirty = true;
-		pew->geometryDirtyMutex.unlock();
-
-		// if the piece already changed, start over
-		pew->tempPieceMutex.lock();
-		pieceChanged = pew->tempPieceDirty;
-		pew->tempPieceMutex.unlock();
-
-		if (pieceChanged)
-		{
-			deep_delete(myTempPiece);
-			goto compute;
-		}
-
-		emit finishedMesh(completed, GlobalGraphicsSetting::VERY_LOW);
-		if (!completed)
-		{
-			deep_delete(myTempPiece);
-			goto sleep;
-		}
-
-		// now we don't launch immediately into the high-res version
-		// the waiting is to avoid annoying flashing, gui blocking
-		// (from copying geometry), and wasted effort in producing a
-		// high-res geometry that will immediately be replaced.
-		//
-		unsigned int delay_msecs = 2000;
-		for (unsigned int i = 0; i < delay_msecs / 250; ++i)
-		{
-			msleep(250);
-			
-			// check if piece has changed and start over if so	
-			pew->tempPieceMutex.lock();
-			pieceChanged = pew->tempPieceDirty;
-			pew->tempPieceMutex.unlock();
-
-			if (pieceChanged)
-			{
-				deep_delete(myTempPiece);
-				goto compute;
-			}
-		}
-
 		// compute the high-res geometry
 		pew->tempGeometryMutex.lock();
 		completed = generateMesh(myTempPiece, &(pew->tempPieceGeometry), &(pew->tempPickupGeometry), 
@@ -93,13 +42,13 @@ void PieceGeometryThread::run()
 		pew->tempPieceMutex.lock();
 		pieceChanged = pew->tempPieceDirty;
 		pew->tempPieceMutex.unlock();
-
 		if (pieceChanged)
 		{
 			deep_delete(myTempPiece);
 			goto compute;
 		}
 
+		// finish and loop, going to sleep
 		deep_delete(myTempPiece);
 		emit finishedMesh(completed, GlobalGraphicsSetting::HIGH);
 	}
--- pullplangeometrythread.cpp
+++ pullplangeometrythread.cpp
@@ -17,8 +17,6 @@ void PullPlanGeometryThread::run()
 	bool completed;
 	while (1)
 	{
-		sleep:
-
 		ppew->wakeWait.wait(&(ppew->wakeMutex));
 
 		compute:
@@ -30,51 +28,6 @@ void PullPlanGeometryThread::run()
 		ppew->tempPullPlanDirty = false;
 		ppew->tempPullPlanMutex.unlock();	
 	
-		// now lock the geometry
-		ppew->tempGeometryMutex.lock();
-		completed = generateMesh(myTempPullPlan, &(ppew->tempGeometry), GlobalGraphicsSetting::MEDIUM);
-		ppew->tempGeometryMutex.unlock();	
-		ppew->geometryDirtyMutex.lock();
-		ppew->geometryDirty = true;
-		ppew->geometryDirtyMutex.unlock();
-
-		ppew->tempPullPlanMutex.lock();
-		pullPlanChanged = ppew->tempPullPlanDirty;
-		ppew->tempPullPlanMutex.unlock();
-		if (pullPlanChanged)
-		{
-			deep_delete(myTempPullPlan);
-			goto compute;
-		}
-		
-		emit finishedMesh(completed, GlobalGraphicsSetting::MEDIUM);
-		if (!completed)
-		{
-			deep_delete(myTempPullPlan);
-			goto sleep;
-		}
-
-		// now we don't launch immediately into the high-res version
-		// the waiting is to avoid annoying flashing, gui blocking
-		// (from copying geometry), and wasted effort in producing a
-		// high-res geometry that will immediately be replaced.
-		//
-		unsigned int delay_msecs = 2000;
-		for (unsigned int i = 0; i < delay_msecs / 250; ++i)
-		{
-			msleep(250);
-
-			// check if piece has changed and start over if so      
-			ppew->tempPullPlanMutex.lock();
-			pullPlanChanged = ppew->tempPullPlanDirty;
-			ppew->tempPullPlanMutex.unlock();
-			if (pullPlanChanged)
-			{
-				deep_delete(myTempPullPlan);
-				goto compute;
-			}
-		}
-
 		// now lock the geometry
 		ppew->tempGeometryMutex.lock();
 		completed = generateMesh(myTempPullPlan, &(ppew->tempGeometry), GlobalGraphicsSetting::VERY_HIGH);
