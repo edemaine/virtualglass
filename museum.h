9c703b54e45920b07fb32042fc3af478848bf6c3
Andrew Winslow <andrewwinslow@gmail.com>
2016-02-22 22:57:06 +0000

    Adding a new header file for things related to the museum version, including a global #define to enable/disable it.

---
+++ museum.h
@@ -0,0 +1,14 @@
+
+#ifndef MUSEUM_H
+#define MUSEUM_H
+
+// Uncomment to switch to the "museum" version of software.
+// This is a version more friendly to settings where passersby,
+// dilettantes, and the general public will be using the software
+#define MUSEUM
+
+// Five minutes of inactivity causes the software to reset
+const int seconds_until_reset = 300; 
+
+#endif
+
